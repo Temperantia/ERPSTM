6f345ee130474314f7f04b3984bf20dc80650c75
Florent <florent.bouisset@gmail.com>
2020-02-20 15:11:45 +0100

    premiere iteration du rens d'info d'entreprise

--- frontend/app.py
+++ frontend/app.py
@@ -8,13 +8,14 @@ from kivy.uix.recycleview import RecycleView
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import Screen, ScreenManager
-
+from kivy.uix.dropdown import DropDown #pour utilisation de menu deroulants
+from kivy.base import runTouchApp
 
 
 def standardTextField(text):
     t = TextInput()
     t.hint_text = text
-    t.font_size = 25
+    t.font_size = 22
     t.focus = True
     t.size_hint_max_y = 40
     t.multiline = False
@@ -103,6 +104,8 @@ class Register(Screen):
         self.pseudo = standardTextField("Pseudo")
         self.passwd = standardTextField("Mot de passe")
 
+        self.passwd.password = True
+
         b.add_widget(self.firstName)
         b.add_widget(self.lastName)
         b.add_widget(self.birthDate)
@@ -119,7 +122,7 @@ class Register(Screen):
         l = Label(text="")
 
         b.add_widget(btn)
-        b.add_widget(l)
+        #b.add_widget(l)
         self.add_widget(b)
 
     def changeStatus(self):
@@ -193,6 +196,61 @@ class PickRegisterOrLogin(Screen):
     def goToRegisterSreen(self):
         MyApp.sm.current = 'register'
 
+class CompanyInformations(Screen):
+    def switchStatutJuridique(self,numero):
+        nom = ""
+        if (numero == 0):
+            nom = "SARL"
+        if (numero == 1):
+            nom = "SA"
+        if (numero == 2):
+            nom = "SAS"
+        if (numero == 3):
+            nom = "EURL"
+        if (numero == 4):
+            nom = "EI" 
+        return nom
+
+
+    def __init__(self, **kwargs):
+        super(CompanyInformations, self).__init__(**kwargs)
+        b = BoxLayout(orientation='vertical')
+        b.padding = [10, 10, 10, 10]
+        b.pos = (0, 0)
+        b.spacing = 40
+        b.size_hint = (0.5,1)
+        b.pos_hint = {'x' : 0.25}
+
+        title = standardTitle("Renseignements des informations d'entreprises")
+
+        dropdown = DropDown()
+        for index in range(5):
+
+            nom = self.switchStatutJuridique(index)
+
+            btn = Button(text = nom, size_hint_y=None, height=44)
+            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+            dropdown.add_widget(btn)
+
+        mainbutton = Button(text='Statut Juridique', size_hint=(1, .5))
+        mainbutton.bind(on_release=dropdown.open)
+        dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+
+
+        turnOver = standardTextField("Chiffre d'affaires en €")
+        
+
+        validatingButton = Button()
+        validatingButton.text = "Valider"
+
+        b.add_widget(title)
+        b.add_widget(mainbutton)
+        b.add_widget(turnOver)
+        b.add_widget(validatingButton)
+        
+
+        self.add_widget(b)
+
 class MyApp(App):
 
     sm = ScreenManager()
@@ -201,11 +259,13 @@ class MyApp(App):
         pickLogin = PickRegisterOrLogin(name = 'pickLogin')
         login = Login(name='login')
         register = Register(name = 'register')
+        companyInformations = CompanyInformations(name = 'companyInformations')
 
         self.sm.add_widget(pickLogin)
         self.sm.add_widget(login)
         self.sm.add_widget(register)
-        #sm.current = 'register'
+        self.sm.add_widget(companyInformations)
+        self.sm.current = 'companyInformations'
         
         return self.sm
 
