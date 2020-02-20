f4bcb69c9acdbcf1987d6ffe69b84fb8b9036c7d
Florent <florent.bouisset@gmail.com>
2020-02-20 13:32:51 +0100

    Taille des boutons ajustés

--- frontend/app.py
+++ frontend/app.py
@@ -53,7 +53,7 @@ class Login(Screen):
         b.add_widget(self.mdp)
 
         btn = Button(text="Valider")
-        btn.size_hint = (1,0.2)
+        btn.size_hint = (1,.4)
         btn.on_press = self.validating
         l = Label(text="")
 
@@ -82,12 +82,14 @@ class Register(Screen):
     birthDate = None
     pseudo = None
     passwd = None
+    buttnSupplierIntern = Button()
+    supplier = True         #true if is a supplier, false if is an intern
     def __init__(self, **kwargs):
         super(Register, self).__init__(**kwargs)
         b = BoxLayout(orientation='vertical')
         b.padding = [10, 10, 10, 10]
         b.pos = (0, 0)
-        b.spacing = 40
+        b.spacing = 20
         b.size_hint = (0.5,1)
         b.pos_hint = {'x' : 0.25} 
 
@@ -107,14 +109,29 @@ class Register(Screen):
         b.add_widget(self.pseudo)
         b.add_widget(self.passwd)
 
+        self.buttnSupplierIntern.text = "Cliquez pour choisir votre statut"
+        self.buttnSupplierIntern.on_press = self.changeStatus
+        
+        b.add_widget(self.buttnSupplierIntern)
+
         btn = Button(text="Valider")
-        btn.size_hint = (1, 0.2)
         btn.on_press = self.validating
         l = Label(text="")
 
         b.add_widget(btn)
         b.add_widget(l)
         self.add_widget(b)
+
+    def changeStatus(self):
+        if(self.supplier):
+            message = "Vous êtes un Interne" +'\n' + "Cliquez pour changer"
+            self.buttnSupplierIntern.text = message
+            self.supplier = False
+
+        else:
+            message = "Vous êtes un Fournisseur" +'\n' + "Cliquez pour changer"
+            self.buttnSupplierIntern.text = message
+            self.supplier = True
 
     def validating(self):
         self.getFields()
